e2b49c0cbbacbf18ccd4960efddd4e534e19732d
Jagrutiahire09 <105353998+Jagrutiahire09@users.noreply.github.com>
2022-05-12 15:27:14 +0530

    Create class_sample.java
    created

---
+++ class_sample.java
@@ -0,0 +1,22 @@
+
+jagruti.sample.java.util.*;
+{
+public class class_sample
+{
+public static void main(string[]args);
+{
+System.out.println("taking input from user");
+Scanner sc = new scanner(system.in);
+System.out.println("enter number 1");
+int a=sc.nextInt();
+System.out.println("enter number2");
+int b=sc.nextInt();
+int Sum=a+b;
+system.out.println("the sum of these numbers");
+system.out.println(sum);
+
+}
+}
+}
+
+}
